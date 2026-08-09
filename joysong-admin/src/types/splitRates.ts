a42396fa6679143c364ca5cdc850b2349c4c1868
shimmail <shimmail@126.com>
2026-08-09 17:31:43 +0800

    test: add admin split rate test foundation

---
+++ joysong-admin/src/types/splitRates.ts
@@ -0,0 +1,9 @@
+export interface OrderSplitPolicy {
+  platformRate: number;
+}
+
+export interface SplitConfigRates {
+  institutionRate: number;
+  /** Compatibility field whose product meaning is medical beauty consultant share. */
+  commissionRate: number;
+}
